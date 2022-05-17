fca3803e55f64e4dfa034e92ce348884d3600f39
Yuqi Liu <yuqi.lyle@outlook.com>
2022-05-17 10:37:47 +0800

    reduce the codes and make it more flexible

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,44 @@ use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 
+use serde::de::value::Error;
 use serde_json::Value;
 
 use web3::contract::{Contract, Options};
 use web3::transports::WebSocket;
 use web3::types::{Address, U256};
 
-fn get_oracle_feeds(token: &str) -> (String, String) {
+fn load_abi_json(json_file: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(json_file).unwrap();
+    let mut json_str = String::new();
+    file.read_to_string(&mut json_str).unwrap();
+
+    Ok(json_str)
+}
+
+fn get_oracle_feeds(token: &str) -> Result<(String, String), Error> {
     let feeds_file = "chainlink-data-feeds.json";
-    let mut file = File::open(feeds_file).unwrap();
-    let mut data = String::new();
-    file.read_to_string(&mut data).unwrap();
+    let data = load_abi_json(feeds_file).unwrap();
 
     let json: Value = serde_json::from_str(&data).unwrap();
     let addr: &str = json[token]["address"].as_str().unwrap();
     let abi: &str = json[token]["abi"].as_str().unwrap();
 
-    (addr.to_string(), abi.to_string())
+    Ok((addr.to_string(), abi.to_string()))
 }
 
-fn load_abi_json(json_file: &str) -> String {
-    let mut file = File::open(json_file).unwrap();
-    let mut json_str = String::new();
-    file.read_to_string(&mut json_str).unwrap();
-
-    json_str
-}
-
-async fn get_price(token: &str) -> f64 {
+async fn get_price(token: &str) -> Result<f64, web3::Error> {
     let wss = "wss://mainnet.infura.io/ws/v3/48c4fb93a3794a1fb80da6c53226db1c";
     let websocket = WebSocket::new(wss).await.unwrap();
     let web3 = web3::Web3::new(websocket);
 
-    let (addr, abi_file) = get_oracle_feeds(token);
+    let (addr, abi_file) = get_oracle_feeds(token).unwrap();
 
     let contract_addr = Address::from_str(addr.as_str()).unwrap();
 
     // get abi, cause include_bytes!() need take a string literal,
     // the macro works at compile time, can not take a variable as parameter
-    let abi_str = load_abi_json(abi_file.as_str());
+    let abi_str = load_abi_json(abi_file.as_str()).unwrap();
     let abi = abi_str.as_bytes();
 
     let contract = Contract::from_json(web3.eth(), contract_addr, abi).unwrap();
@@ -55,11 +54,11 @@ async fn get_price(token: &str) -> f64 {
         .await
         .unwrap();
 
-    price_result.1 as f64 / i64::pow(10, decimals) as f64
+    Ok(price_result.1 as f64 / i64::pow(10, decimals) as f64)
 }
 
 #[tokio::main]
 async fn main() {
-    let price = get_price("btc").await;
+    let price = get_price("btc").await.unwrap();
     println!("Current Price of BTC is: {}", price);
 }
